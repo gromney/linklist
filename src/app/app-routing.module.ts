4c07d88010eb6de708bfe7026e6ab1739939b8c7
Geronimo Romney <g.romney@gmail.com>
2021-10-01 11:28:56 +0800

    modificando las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { CollectionsComponent } from './list/collections/collections.component';
 import { EditListComponent } from './list/edit-list/edit-list.component';
+import { ViewListComponent } from './list/view-list/view-list.component';
 
 const routes: Routes = [
   {
     path: '', component: LayoutComponent, children: [
-      { path: 'edit', component: EditListComponent,data: {} },
-      { path: 'collections', component: CollectionsComponent },
-      { path: '**', redirectTo: 'edit' }
+      { path: 's/edit', component: EditListComponent },
+      { path: 's/collections', component: CollectionsComponent },
+      { path: ':title', component: ViewListComponent },
+      { path: '**', redirectTo: 's/edit' }
     ]
   }
+  
 ];
 
 @NgModule({
